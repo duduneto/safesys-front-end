0197db750d02d0d10a2940ea57d0497fa2dc1f08
duduneto <31359405+duduneto@users.noreply.github.com>
2018-03-16 12:40:54 -0300

    Urls

--- src/common/urls.js
+++ src/common/urls.js
@@ -1,4 +1,4 @@
 export default{
-    API_URL: 'http://localhost:3030/api',
-    OAPI_URL: 'http://localhost:3030/oapi'
+    API_URL: 'https://safesys-api.herokuapp.com/api',
+    OAPI_URL: 'https://safesys-api.herokuapp.com/oapi'
 }
